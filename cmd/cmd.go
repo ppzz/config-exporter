ab84645e834b2ef3ce933c603743cbee47b45466
ppzz <penn.chn@gmail.com>
2024-04-17 18:02:53 +0800

    style: name change

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -13,7 +13,6 @@ var cfgFile string
 
 var descLong = `游戏配置表的处理程序,包括: Excel转Csv, Csv 过滤, 根据 Csv 生成 Go 代码`
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:              "golang-csv",
 	Short:            "关于csv的配置表处理",
@@ -21,8 +20,6 @@ var rootCmd = &cobra.Command{
 	TraverseChildren: true,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -32,15 +29,7 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is ./csv-gen.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is ./config-exporter.yaml)")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -49,20 +38,16 @@ func initConfig() {
 	envPrefix := "APP"
 	envReplacer := strings.NewReplacer(".", "_")
 
-	defCfgName := "csv-gen"
+	defCfgName := "config-exporter"
 	defCfgExt := "yaml"
 	defCfgDir := "."
 
 	filePath := path.Join(defCfgDir, defCfgName+"."+defCfgExt)
 	if cfgFile != "" {
-		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 		err := viper.ReadInConfig()
 		cobra.CheckErr(err)
 	} else if ExistFile(filePath) {
-		// Find home directory.
-		// home, err := os.UserHomeDir()
-		// cobra.CheckErr(err)
 		viper.SetConfigName(defCfgName)
 		viper.SetConfigType(defCfgExt)
 		viper.AddConfigPath(defCfgDir)
